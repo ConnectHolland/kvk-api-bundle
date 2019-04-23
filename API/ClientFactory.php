a6c4f0728e4aca33f5315d4bed49c98d0fcec0a9
Matthijs Hasenpflug <matthijs@connectholland.nl>
2019-04-23 15:16:40 +0200

    Add middleware to handle authentication and create bundle configuration

--- API/ClientFactory.php
+++ API/ClientFactory.php
@@ -10,12 +10,43 @@ declare(strict_types=1);
 namespace ConnectHolland\KvkApiBundle\API;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Middleware;
 use Http\Adapter\Guzzle6\Client as HttpClient;
+use Psr\Http\Message\RequestInterface;
 
 class ClientFactory
 {
-    public static function createApiClient(ClientInterface $client): Client
+    public static function createApiClient(ClientInterface $client, string $apiToken, string $testUsername, string $testPassword, bool $useTestingAccount): Client
     {
+        $client->getConfig('handler')->unshift(Middleware::mapRequest(function (RequestInterface $request) use ($apiToken, $testUsername, $testPassword, $useTestingAccount) {
+            if ($useTestingAccount) {
+                return self::addTestingAuthentication($request, $testUsername, $testPassword);
+            }
+
+            return self::addApiToken($request, $apiToken);
+        }));
+
         return Client::create(new HttpClient($client));
+    }
+
+    /**
+     * Add testing authentication.
+     */
+    private static function addTestingAuthentication(RequestInterface $request, string $testUsername, string $testPassword): RequestInterface
+    {
+        return $request->withHeader('Authorization', sprintf('Basic %s', base64_encode($testUsername.':'.$testPassword)));
+    }
+
+    /**
+     * Add api token for production.
+     */
+    private static function addApiToken(RequestInterface $request, string $apiToken): RequestInterface
+    {
+        $uri = $request->getUri();
+        parse_str($uri->getQuery(), $query);
+        $query['user_key'] = $apiToken;
+        $uri               = $uri->withQuery(http_build_query($query));
+
+        return $request->withUri($uri);
     }
 }
